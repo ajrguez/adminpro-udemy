7412d5dc68caba8183bc13217f90e7286be7bff0
AJ <ajrguez@ajsystems.es>
2018-03-09 18:25:44 +0000

    Creada estructura de sitio. login, pages(dashboard, graficas1 y progress), servicios (shared y sidebar), y elementos compartidos  en shared (header,breadcrumb,nopagefound y sidebar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
+// Importamos Módulos
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+// Importamos Componentes
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+
+// Importamos Servicios
+import { SharedService } from './services/shared.service';
+import { SidebarService } from './services/sidebar.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    SharedService,
+    SidebarService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
